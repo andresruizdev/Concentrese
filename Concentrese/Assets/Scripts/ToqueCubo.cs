77262ebc93e1a892810ba88d559cadbc428b6b4f
Andres Ruiz <rb.andres98@gmail.com>
2017-10-10 21:35:50 -0500

    Control Scene

---
+++ Concentrese/Assets/Scripts/ToqueCubo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class ToqueCubo : MonoBehaviour
+{
+    Animation anim;
+    private void Start()
+    {
+        anim = GetComponent<Animation>();
+    }
+    private void OnMouseDown()
+    {
+        print("entra");
+    }
+
+}
