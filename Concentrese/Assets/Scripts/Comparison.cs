465480952473703e953fca47f3dd97d7a3160227
Andres Ruiz <rb.andres98@gmail.com>
2017-10-11 21:56:30 -0500

    Basic Animations Implemented

---
+++ Concentrese/Assets/Scripts/Comparison.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comparison : MonoBehaviour {
+    public static int pos = 0;
+    public static GameObject[] seleccionados = new GameObject[2];
+}
--- Concentrese/Assets/Scripts/ToqueCubo.cs
+++ Concentrese/Assets/Scripts/ToqueCubo.cs
@@ -5,14 +5,52 @@ using UnityEngine.Animations;
 
 public class ToqueCubo : MonoBehaviour
 {
-    Animation anim;
-    private void Start()
+    Animator anim;
+    bool hidden = true;
+    private void Awake()
     {
-        anim = GetComponent<Animation>();
+        anim = transform.parent.GetComponent<Animator>();
     }
-    private void OnMouseDown()
+
+    private void Update()
     {
-        print("entra");
+        if (hidden)
+        {
+            hidden = false;
+
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        if (hidden && Comparison.pos < 2)
+        {
+            PlayAnimation();
+            hidden = false;
+            Comparison.seleccionados[Comparison.pos] = this.gameObject;
+            Comparison.pos++;
+        }
+        else if (!hidden)
+        {
+            PlayAnimation();
+            hidden = true;
+            Comparison.pos--;
+        }
+        else if (Comparison.pos >= 2)
+        {
+            anim.SetBool("Block", true);
+            Invoke("BlockFalse", 1f);
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        anim.SetBool("Showing", hidden);
+    }
+
+    void BlockFalse()
+    {
+        anim.SetBool("Block", false);
     }
 
 }
