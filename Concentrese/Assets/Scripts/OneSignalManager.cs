c5b11960ddc1d1075548647bad5513512fea2709
Andres Ruiz <rb.andres98@gmail.com>
2017-11-08 01:15:18 -0500

    OneSignal

---
+++ Concentrese/Assets/Scripts/OneSignalManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic; // Esta es la libreria que requiere.
+using UnityEngine;
+
+public class OneSignalManager : MonoBehaviour
+{
+	void Start ()
+    {
+        OneSignal.StartInit("827ddd42-331d-4981-a0cb-e5163d7a16e7").HandleNotificationOpened(HandleNotificationOpened).EndInit();
+	}
+
+    private static void HandleNotificationOpened(OSNotificationOpenedResult result) { }
+	
+}
